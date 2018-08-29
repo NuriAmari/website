6af5b846c28473ce92aeb332f4c9f421f487bb61
Nuri <nuri.amari99@gmail.com>
2018-08-29 11:41:30 -0400

    Project links updated

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () =>
         </p>
         <div className="projects-list">
             <div className="card">
-                <h2>Personal Website <a href="https://github.com/NuriAmari/MajorIncidentManagement" rel="noopener noreferrer" target="_blank">
+                <h2>Personal Website <a href="https://github.com/NuriAmari/react-personal-website" rel="noopener noreferrer" target="_blank">
                     <img src={require("../../img/github.svg")} alt=""></img>
                 </a></h2>
                 
@@ -33,7 +33,10 @@ const Projects = () =>
                 <h2>Incident Management <a href="https://github.com/NuriAmari/MajorIncidentManagement" rel="noopener noreferrer" target="_blank">
                     <img src={require("../../img/github.svg")} alt=""></img>
                 </a></h2>
-                <p> 
+                <p>During my time working at York Region, I became a little frustrated with some of the tools I had to use. In my free time, I rebuilt a work project, but
+                    this time with modern tools I liked better. I eventually presented the project to my supervisor and was able to continue working with the stack for the remainder of my time
+                    there. The Region is now investigating using the stack full time and a presentation I prepared has been recorded and circulated throughout the region. The web app I created
+                    is an incident management tool that focuses on real time and responsive behaviour. 
                 </p>
                 <ul>
                     <li>NodeJS</li>
@@ -46,23 +49,32 @@ const Projects = () =>
                 </ul>
             </div>
             <div className="card">
-                <h2>BMO myApp <a href="https://github.com/NuriAmari/MajorIncidentManagement" rel="noopener noreferrer" target="_blank">
+                <h2>BMO myApp <a href="https://github.com/NuriAmari/BMOApp" rel="noopener noreferrer" target="_blank">
                     <img src={require("../../img/github.svg")} alt=""></img>
                 </a></h2>
-                
+                <p>For a high school business competition, my partner and I created a proof of concept mobile banking Android application capable of reading and writing
+                    data to NFC data chips. As part of a marketing campaign, we design and produced physical NFC enabled posters allowing customers to interact with our advertisements.
+                </p>
+                <ul>
+                    <li>Java</li>
+                    <li>Android Studio</li>
+                    <li>NFC API</li>
+                    <li>Adobe Animate</li>
+                    <li>Adobe Photoshop</li>
+                </ul>
             </div>
             <div className="card">
-                <h2>Flat Colours <a href="https://github.com/NuriAmari/MajorIncidentManagement" rel="noopener noreferrer" target="_blank">
+                <h2>Flat Colours <a href="https://github.com/NuriAmari/Flat-Colours" rel="noopener noreferrer" target="_blank">
                     <img src={require("../../img/github.svg")} alt=""></img>
                 </a></h2>
             </div>
             <div className="card">
-                <h2>Nodejs Snake <a href="https://github.com/NuriAmari/MajorIncidentManagement" rel="noopener noreferrer" target="_blank">
+                <h2>Nodejs Snake <a href="https://github.com/NuriAmari/Snake.io" rel="noopener noreferrer" target="_blank">
                     <img src={require("../../img/github.svg")} alt=""></img>
                 </a></h2>
             </div>
             <div className="card">
-                <h2>RPG Adventure <a href="https://github.com/NuriAmari/MajorIncidentManagement" rel="noopener noreferrer" target="_blank">
+                <h2>RPG Adventure <a href="https://github.com/NuriAmari/Little-RPG-Adventure" rel="noopener noreferrer" target="_blank">
                     <img src={require("../../img/github.svg")} alt=""></img>
                 </a></h2>
             </div>

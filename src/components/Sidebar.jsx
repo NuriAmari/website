220b17da5fdcebcadcf58c8f99891cd28660eb60
NuriAmari <nuri.amari99@gmail.com>
2019-01-14 16:49:57 -0500

    updated sidebar information

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,48 +2,78 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as routes from '../constants/routes';
 
-const Sidebar = (props) =>
+const Sidebar = props => (
     <div className="Sidebar">
-        <Navigation toggleMenu={ props.toggleMenu }/>
+        <Navigation toggleMenu={props.toggleMenu} />
     </div>
+);
 
-const Navigation = (props) => {
-     
+const Navigation = props => {
     const { toggleMenu } = props;
 
     return (
         <React.Fragment>
             <ul className="Navigation">
                 <li>
-                    <Link style={navStyle} to={routes.HOME} onClick={toggleMenu}>Home</Link>
+                    <Link
+                        style={navStyle}
+                        to={routes.HOME}
+                        onClick={toggleMenu}
+                    >
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link style={navStyle} to={routes.ABOUT} onClick={toggleMenu}>About</Link>
+                    <Link
+                        style={navStyle}
+                        to={routes.ABOUT}
+                        onClick={toggleMenu}
+                    >
+                        About
+                    </Link>
                 </li>
                 <li>
-                    <Link style={navStyle} to={routes.PROJECTS} onClick={toggleMenu}>Projects</Link>
+                    <Link
+                        style={navStyle}
+                        to={routes.PROJECTS}
+                        onClick={toggleMenu}
+                    >
+                        Projects
+                    </Link>
                 </li>
                 <li>
-                    <Link style={navStyle} to={routes.EXPERIENCE} onClick={toggleMenu}>Experience</Link>
+                    <Link
+                        style={navStyle}
+                        to={routes.EXPERIENCE}
+                        onClick={toggleMenu}
+                    >
+                        Experience
+                    </Link>
                 </li>
             </ul>
             <div className="sidebar-connect">
                 <ul>
                     <li>Nuri Amari</li>
-                    <li>2A CS @ UWaterloo</li>
-                    <li><a href="tel:6133051225">613 305 1225</a></li>
-                    <li><a href="mailto:nfamari@edu.uwaterloo.ca">nfamari@edu.uwaterloo.ca</a></li>
+                    <li>2B CS @ UWaterloo</li>
+                    <li>
+                        <a href="tel:6133051225">613 305 1225</a>
+                    </li>
+                    <li>
+                        <a href="mailto:nfamari@edu.uwaterloo.ca">
+                            nfamari@edu.uwaterloo.ca
+                        </a>
+                    </li>
                 </ul>
             </div>
         </React.Fragment>
     );
-}
+};
 
 const navStyle = {
     color: '#313131',
     fontFamily: 'sans-serif',
     fontWeight: 'Bold',
     fontSize: '6vh',
-}
+};
 
 export default Sidebar;
